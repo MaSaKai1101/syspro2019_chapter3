8241c67d6bb366ae79f5847b457f0f5542eaad4d
MaSaKai1101 <shatanaka7@gmail.com>
2019-09-26 02:29:01 +0100

    fix count out

--- ledblink.py
+++ ledblink.py
@@ -2,8 +2,13 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT)
+
+i = 0
 while True:
+    i = i + 1
     GPIO.output(8, True)
     time.sleep(1)
     GPIO.output(8, False)
     time.sleep(1)
+    if i == 5:
+ 	break;
